cfbfa2e631b89f58fa3bf07feaeb74513837797b
Ritam Roy <ritamroy380@gmail.com>
2022-08-17 21:31:05 +0530

    Web Development Starter Pack

---
+++ JavaScript/first/first.js
@@ -0,0 +1,29 @@
+alert("open console");
+
+var a = 69;
+var b = "Noob";
+c = 420.69;
+var num1 = 96;
+var num2 = 27;
+
+console.log(c);
+console.log(a);
+console.log(b);
+
+console.log("");
+
+console.log("num1 = " + num1, "    ", "num2 = " + num2);
+
+console.log("");
+
+console.log("The value of num1 + num2 is " + (num1 + num2));
+console.log("The value of num1 - num2 is " + (num1 - num2));
+console.log("The value of num1 x num2 is " + num1 * num2);
+console.log("The value of num1 ÷ num2 is " + num1 / num2);
+
+console.log("");
+
+console.log("num1-- : " + num1--, "       ", "num2-- : " + num2--);
+console.log("--num1 : " + --num1, "       ", "--num2 : " + --num2);
+console.log("num1++ : " + num1++, "       ", "num2++ : " + num2++);
+console.log("++num1 : " + ++num1, "       ", "++num2 : " + ++num2);
---
+++ JavaScript/second/second.js
@@ -0,0 +1,51 @@
+console.clear();
+console.log("Hello Noob");
+alert("Alert for no reason");
+document.write("this is document write via Javascript");
+
+// Javascript console API
+console.warn("Giving warning for no reason");
+console.error("Error occured for no reason");
+console.assert(4 == 2 - 2);
+
+// Javascript variables
+var num1 = 420;
+var num2 = 69;
+console.log(num1 - num2);
+
+// data types in javascript
+//
+// numbers
+var num3 = 455;
+var num4 = 69.96;
+//
+// string
+var str1 = "This is a string";
+var str2 = "This is also a string";
+//
+// object
+var marks = {
+  noob: 34,
+  bot: 69,
+  pro: 99.99,
+};
+//
+// Boolean
+var a = true;
+var b = false;
+//
+// undefined ,  null
+var yes;
+var no = null;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(str1);
+console.log(str2);
+console.log(marks);
+console.log(a, b);
+console.log(yes);
+console.log(no);
+console.log("Hello\n NooB");
